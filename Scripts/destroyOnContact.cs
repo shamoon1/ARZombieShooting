507229992068d6acc121fc5b5eee01b716f44f36
shamoon1 <60936300+shamoon1@users.noreply.github.com>
2020-02-17 08:51:04 +0500

    Add files via upload

---
+++ Scripts/destroyOnContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("destroyOnContacts", 2f);
+    }
+    void destroyOnContacts()
+    {
+        Destroy(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
